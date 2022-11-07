4eddf9039a8388b5a7e9c7b70e9c8cf9d89de5cc
logicjohn <undieingspirit@gmail.com>
2022-11-07 04:16:20 +0000

    test

--- poke-query/main.py
+++ poke-query/main.py
@@ -1,39 +1,39 @@
-# Flask Setup
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
-from flask import Flask, jsonify, request, abort
-from flask import render_template
+
+import requests
+import json
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-# Google Sheets API Setup
-credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json",
-                                                              ["https://spreadsheets.google.com/feeds",
-                                                               "https://www.googleapis.com/auth/spreadsheets",
-                                                               "https://www.googleapis.com/auth/drive.file",
-                                                               "https://www.googleapis.com/auth/drive"])
 
-client = gspread.authorize(credential)
-gsheet = client.open("Hungry in Chapel Hill").sheet1
-# An example GET Route to get all reviews
+@app.route('/')
+def index():
+    return 'Add a pokemon name to the end of the url\n'
 
+@app.route('/<pokemonName>')
+def index2(pokemonName):
 
-@app.route('/test')
-def all_reviews():
-    print(gsheet.get_all_records())
-    return jsonify(gsheet.get_all_records())
-@app.route("/")
-def hello_world():
-    print(gsheet.get_all_records())
-    #print(gsheet.get_all_records()[0]['Timestamp'])
-    timestamps = []
-    for x in gsheet.get_all_records():
-        timestamps.append(x['Timestamp'])
-    return render_template('index.html', test=timestamps)
+    pokemontypes = {
+        "name" : None,
+        "type1": None,
+        "type2": None
+    }
+    
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemonName}"
+    response = requests.get(url)
+    print(response.status_code)
+    print("Pokemon name: " + response.json()['forms'][0]['name'])
 
-@app.route('/temp')
-def temp():
-    print("temp")
-    return "This is working \n"
+    pokemontypes["name"] = response.json()['forms'][0]['name']
+
+    for n in response.json()['types']:
+        print(n['type']['name'])
+        if pokemontypes['type1'] == None:
+            pokemontypes['type1']= n['type']['name']
+        else:
+            pokemontypes['type2'] = n['type']['name']
+    
+    return pokemontypes
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
 
--- spotify-server/server.js
+++ spotify-server/server.js
@@ -4,29 +4,7 @@ const app = express()
 
 const request = require('request');
 
-const https = require('https')
-
 const port = 5111
-
-var SpotifyWebApi = require('spotify-web-api-node');
-
-// credentials are optional
-
-let  client_id = 'x'
-let  client_secret = 'x'
-let  redirectUri = 'http://www.example.com/callback'
-
-
-var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-    },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
-  };
 
 const dict = {
     "fire": "rock",
@@ -37,14 +15,6 @@ const dict = {
 // res.json({spotifyuri: ''})
 
 app.get('/', (req, res) => {
-
-    request.post(authOptions, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          var token = body.access_token;
-          checkSpotify(token, 'rock')
-        }
-      });
-
     res.send("spotify api server is listening\n")
 })
 
@@ -63,28 +33,14 @@ app.get('/type1/:type1/type2/:type2', (req, res) => {
 })
 
 function pickMusic(pokemonType) {
-
      return dict[pokemonType]
 }
 
-function checkSpotify(token, genre) {
-    let url = "https://api.spotify.com/v1/" 
-    url = url + "recommendations/available-genre-seeds"
-    token = "Bearer " + token
-    var options = {
-        'method': 'GET',
-        'url'  : url,
-        'headers': {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-        }
-    }
+function checkSpotify(genre) {
+    let url = "https://api.spotify.com" 
 
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
-        console.log(response.body)
-    })
 }
+
 
 app.listen(port, () => {
     console.log(`Spotify server is listening on port ${port}`)
