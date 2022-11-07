feb4bd93152224be29120f54d52d7e6b29fa66c0
logicjohn <undieingspirit@gmail.com>
2022-11-07 03:56:38 +0000

    latest

--- spotify-server/server.js
+++ spotify-server/server.js
@@ -4,7 +4,29 @@ const app = express()
 
 const request = require('request');
 
+const https = require('https')
+
 const port = 5111
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+
+let  client_id = 'x'
+let  client_secret = 'x'
+let  redirectUri = 'http://www.example.com/callback'
+
+
+var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
 
 const dict = {
     "fire": "rock",
@@ -15,6 +37,14 @@ const dict = {
 // res.json({spotifyuri: ''})
 
 app.get('/', (req, res) => {
+
+    request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var token = body.access_token;
+          checkSpotify(token, 'rock')
+        }
+      });
+
     res.send("spotify api server is listening\n")
 })
 
@@ -33,14 +63,28 @@ app.get('/type1/:type1/type2/:type2', (req, res) => {
 })
 
 function pickMusic(pokemonType) {
+
      return dict[pokemonType]
 }
 
-function checkSpotify(genre) {
-    let url = "https://api.spotify.com" 
+function checkSpotify(token, genre) {
+    let url = "https://api.spotify.com/v1/" 
+    url = url + "recommendations/available-genre-seeds"
+    token = "Bearer " + token
+    var options = {
+        'method': 'GET',
+        'url'  : url,
+        'headers': {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+    }
 
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body)
+    })
 }
-
 
 app.listen(port, () => {
     console.log(`Spotify server is listening on port ${port}`)
